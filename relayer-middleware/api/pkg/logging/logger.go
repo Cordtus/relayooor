6051874b0e7ead987751e9be47f8938a14fd80d8
Cordt Hanson <cordt@cordts-macbook-pro.lan>
2025-07-13 10:53:23 -0600

    Add structured logging with Zap

---
+++ relayer-middleware/api/pkg/logging/logger.go
@@ -0,0 +1,68 @@
+package logging
+
+import (
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var Logger *zap.Logger
+
+func InitLogger(env string) error {
+	var config zap.Config
+
+	if env == "production" {
+		config = zap.NewProductionConfig()
+		config.EncoderConfig.TimeKey = "timestamp"
+		config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	} else {
+		config = zap.NewDevelopmentConfig()
+		config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	}
+
+	// Set log level from environment
+	logLevel := os.Getenv("LOG_LEVEL")
+	if logLevel != "" {
+		var level zapcore.Level
+		if err := level.UnmarshalText([]byte(logLevel)); err == nil {
+			config.Level = zap.NewAtomicLevelAt(level)
+		}
+	}
+
+	logger, err := config.Build(
+		zap.AddCaller(),
+		zap.AddStacktrace(zapcore.ErrorLevel),
+	)
+	if err != nil {
+		return err
+	}
+
+	Logger = logger
+	return nil
+}
+
+// Helper functions for structured logging
+func With(fields ...zap.Field) *zap.Logger {
+	return Logger.With(fields...)
+}
+
+func Info(msg string, fields ...zap.Field) {
+	Logger.Info(msg, fields...)
+}
+
+func Debug(msg string, fields ...zap.Field) {
+	Logger.Debug(msg, fields...)
+}
+
+func Warn(msg string, fields ...zap.Field) {
+	Logger.Warn(msg, fields...)
+}
+
+func Error(msg string, fields ...zap.Field) {
+	Logger.Error(msg, fields...)
+}
+
+func Fatal(msg string, fields ...zap.Field) {
+	Logger.Fatal(msg, fields...)
+}
